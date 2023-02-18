9911dabe26247df0abd05344b7d8a92c913d3f0f
billy rennekamp <billy.rennekamp@gmail.com>
2023-02-19 12:08:24 +1300

    added vector style generation

--- netlify/functions/render/p5.js
+++ netlify/functions/render/p5.js
@@ -1,23 +1,33 @@
 function setup() {
   width = 400
-  margin = 10
-  minLen = 50
-  maxLen = 50
-  strokeW = 40
-  limit = 60
+  margin = 20
+  minLen = width / 10
+  maxLen = minLen
+  strokeW = width / 15
+  limit = 35
   infiniteEdges = false
   searchAttempts = 200
   createCanvas(width, width);
+
   //start in middle of screen
   x = width / 2;
-  y = width / 2;
+  y = width - minLen;
 
+  maxPreviousDirs = 1
   previousDirs = []
   //make the simulation faster
   frameRate(10);
   background(220);
   totalLength = 0
+  line(width / 2, 0, width / 2, width)
+  line(0, width / 2, width, width / 2)
+  line(0, 0, width, width)
+  line(0, width, width, 0)
+  start1 = random(0, 255)
+  start2 = random(0, 255)
+  start3 = random(0, 255)
 }
+
 
 function draw() {
   totalLength++
@@ -26,6 +36,122 @@ function draw() {
   }
   strokeWeight(strokeW);
   stroke(random(0, 255), random(0, 255), random(0, 255));
+  var percentageOfTotal255 = ((totalLength / limit) * 255)
+  // stroke(
+  //   (start1 - percentageOfTotal255),
+  //   (start2 - percentageOfTotal255),
+  //   (start3 - percentageOfTotal255),
+  // );
+  // randomXY()
+  randomVector()
+}
+
+
+function randomVector() {
+  // pick a direction
+  // make sure it's not too close to the previous direction
+  // if the wall is nearby, allow it to go closer to previous direction
+  // pick a length
+  // calculate the x,y of the new point
+  // draw the line
+  let { ang, L, x2, y2 } = pickDir(x, y, previousDirs)
+  // console.log({ ang, L, x2, y2 })
+  line(x, y, x2, y2)
+  x = x2
+  y = y2
+}
+
+function pickDir(x1, y1, previousDirs) {
+  // console.log({ previousDirs })
+  var emergencyLimit = 10000
+  var numberOfTries = 10000
+  var reduceBy = 0
+  var foundAng = false
+  while (!foundAng) {
+    emergencyLimit -= 1
+    if (emergencyLimit < 0) {
+      console.log('emergency limit reached')
+      break
+    }
+    // ang = Math.ceil(random(0, 8)) * 45
+    ang = Math.ceil(random(0, 360))
+
+    var previewX = x1 + Math.cos(ang * Math.PI / 180) * (maxLen)
+    var previewY = y1 + Math.sin(ang * Math.PI / 180) * (maxLen)
+    if (
+      previewX > (width - (maxLen))
+      ||
+      previewX < (0 + (maxLen))
+      ||
+      previewY > (width - (maxLen))
+      ||
+      previewY < (0 + (maxLen))
+    ) {
+      continue
+    }
+
+    if (previousDirs.length == 0) {
+      foundAng = true
+      break
+    }
+    if (previousDirs.length - reduceBy < 0) {
+      console.log('emergency limit reached')
+      break
+    }
+    for (var i = 0; i < previousDirs.length; i++) {
+      var previousDir = previousDirs[i]
+      // var invertedPreviousDir = (previousDir + 180) % 360
+      var distance = Math.abs(modulo(previousDir, 360) - modulo(ang, 360))
+      distance = Math.min(distance, 360 - distance)
+
+
+      if (distance < 60) {
+        // console.log(`distance between ${previousDir} and ${ang} is ${distance} which is less than 90`)
+        foundAng = true
+        break
+      }
+    }
+    if (!foundAng) {
+      numberOfTries -= 1
+      if (numberOfTries < 0) {
+        console.log(`previousDirs are ${previousDirs.toString()}`)
+        console.log(`ang is ${ang} and distance is ${distance} which is more than 90`)
+        numberOfTries = 5
+        reduceBy += 1
+      }
+    }
+  }
+  // console.log(`found ang is ${ang}`)
+  // console.log(`distance between ${previousDir} and ${ang} is ${distance}`)
+
+  previousDirs.push(ang)
+  // console.log(previousDirs.toString())
+  if (previousDirs.length > maxPreviousDirs) {
+    previousDirs.shift()
+  }
+
+
+
+  L = Math.ceil(random(minLen - 1, maxLen))
+  x2 = x1 + Math.cos(ang * Math.PI / 180) * L
+  y2 = y1 + Math.sin(ang * Math.PI / 180) * L
+
+  if (x2 > width) {
+    x2 = width
+  } else if (x2 < 0) {
+    x2 = 0
+  }
+  if (y2 > width) {
+    y2 = width
+  } else if (y2 < 0) {
+    y2 = 0
+  }
+  return { ang, L, x2, y2 }
+}
+
+
+
+function randomXY() {
 
   let foundNextMove = false
   let preventInfLoop = 0
